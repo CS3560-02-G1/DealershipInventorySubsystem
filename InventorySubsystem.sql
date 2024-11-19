f258e1ec6bb94e699032ee3f75a24c19a973de54
Julian Fong <103627958+gragbag@users.noreply.github.com.>
2024-11-18 17:09:16 -0800

    Added the basic SQL Database Export and remove Inventory and Delivery
    Removed Inventory and DelvieryOrder Classes because they are not
    necessary and hard to handle. Included the file for the basic SQL
    Database we will use, with some pre-inserted data for Maintenance,
    Customer, and SalesAgent.

---
+++ InventorySubsystem.sql
@@ -0,0 +1,307 @@
+CREATE DATABASE  IF NOT EXISTS `inventorysubsystem` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `inventorysubsystem`;
+-- MySQL dump 10.13  Distrib 8.0.40, for Win64 (x86_64)
+--
+-- Host: localhost    Database: inventorysubsystem
+-- ------------------------------------------------------
+-- Server version	8.4.3
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `commission`
+--
+
+DROP TABLE IF EXISTS `commission`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `commission` (
+  `TransactionID` int NOT NULL,
+  `AgentID` int NOT NULL,
+  `commissionRate` double NOT NULL,
+  `paymentDate` varchar(30) NOT NULL,
+  PRIMARY KEY (`TransactionID`,`AgentID`),
+  KEY `AgentID` (`AgentID`),
+  CONSTRAINT `commission_ibfk_1` FOREIGN KEY (`TransactionID`) REFERENCES `transaction` (`TransactionID`) ON DELETE CASCADE,
+  CONSTRAINT `commission_ibfk_2` FOREIGN KEY (`AgentID`) REFERENCES `salesagent` (`AgentID`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `commission`
+--
+
+LOCK TABLES `commission` WRITE;
+/*!40000 ALTER TABLE `commission` DISABLE KEYS */;
+/*!40000 ALTER TABLE `commission` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `customer`
+--
+
+DROP TABLE IF EXISTS `customer`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `customer` (
+  `CustomerID` int NOT NULL AUTO_INCREMENT,
+  `firstName` varchar(30) NOT NULL,
+  `lastName` varchar(30) NOT NULL,
+  `phoneNumber` varchar(20) NOT NULL,
+  `address` varchar(50) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  PRIMARY KEY (`CustomerID`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `customer`
+--
+
+LOCK TABLES `customer` WRITE;
+/*!40000 ALTER TABLE `customer` DISABLE KEYS */;
+INSERT INTO `customer` VALUES (1,'Bob','Smith','123 456-7890','123 Abc Drive, CA, USA','bsmith@gmail.com'),(2,'Dean','Bullock','777 777-7777','1012 West Street, CA, USA','deanb@gmail.com'),(3,'Sophia','Car','818 269-3039','4230 Green Street, CA, USA','sophiacar@gmail.com');
+/*!40000 ALTER TABLE `customer` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `lease`
+--
+
+DROP TABLE IF EXISTS `lease`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `lease` (
+  `TransactionID` int NOT NULL,
+  `leasePeriod` int NOT NULL,
+  `monthlyFee` int NOT NULL,
+  PRIMARY KEY (`TransactionID`),
+  CONSTRAINT `lease_ibfk_1` FOREIGN KEY (`TransactionID`) REFERENCES `transaction` (`TransactionID`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `lease`
+--
+
+LOCK TABLES `lease` WRITE;
+/*!40000 ALTER TABLE `lease` DISABLE KEYS */;
+/*!40000 ALTER TABLE `lease` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `maintenance`
+--
+
+DROP TABLE IF EXISTS `maintenance`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `maintenance` (
+  `MaintenanceID` int NOT NULL AUTO_INCREMENT,
+  `type` varchar(50) NOT NULL,
+  `details` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`MaintenanceID`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `maintenance`
+--
+
+LOCK TABLES `maintenance` WRITE;
+/*!40000 ALTER TABLE `maintenance` DISABLE KEYS */;
+INSERT INTO `maintenance` VALUES (1,'Battery Check','Test if the battery is still in good condition'),(2,'Brake Inspection','Check if the brakes and brake pads function correctly'),(3,'Tire Rotation','Rotate the tires to ensure they are in good condition');
+/*!40000 ALTER TABLE `maintenance` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `sale`
+--
+
+DROP TABLE IF EXISTS `sale`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `sale` (
+  `TransactionID` int NOT NULL,
+  `downPayment` int NOT NULL,
+  PRIMARY KEY (`TransactionID`),
+  CONSTRAINT `sale_ibfk_1` FOREIGN KEY (`TransactionID`) REFERENCES `transaction` (`TransactionID`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `sale`
+--
+
+LOCK TABLES `sale` WRITE;
+/*!40000 ALTER TABLE `sale` DISABLE KEYS */;
+/*!40000 ALTER TABLE `sale` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `salesagent`
+--
+
+DROP TABLE IF EXISTS `salesagent`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `salesagent` (
+  `AgentID` int NOT NULL AUTO_INCREMENT,
+  `firstName` varchar(20) NOT NULL,
+  `lastName` varchar(20) NOT NULL,
+  `email` varchar(30) NOT NULL,
+  `phoneNumber` varchar(20) NOT NULL,
+  PRIMARY KEY (`AgentID`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `salesagent`
+--
+
+LOCK TABLES `salesagent` WRITE;
+/*!40000 ALTER TABLE `salesagent` DISABLE KEYS */;
+INSERT INTO `salesagent` VALUES (1,'Amy','Lindsey','amylind12@gmail.com','818 503-5530'),(2,'Edmund','Hale','ehale7@gmail.com','747 907-9981'),(3,'Marshall','Richards','marshallr@gmail.com','818 576-6691');
+/*!40000 ALTER TABLE `salesagent` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `servicerecord`
+--
+
+DROP TABLE IF EXISTS `servicerecord`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `servicerecord` (
+  `RecordID` int NOT NULL AUTO_INCREMENT,
+  `VIN` varchar(17) NOT NULL,
+  `MaintenanceID` int NOT NULL,
+  `date` varchar(20) NOT NULL,
+  `price` int NOT NULL,
+  `status` varchar(50) NOT NULL,
+  PRIMARY KEY (`RecordID`),
+  KEY `VIN` (`VIN`),
+  KEY `MaintenanceID` (`MaintenanceID`),
+  CONSTRAINT `servicerecord_ibfk_1` FOREIGN KEY (`VIN`) REFERENCES `vehicle` (`VIN`) ON DELETE CASCADE,
+  CONSTRAINT `servicerecord_ibfk_2` FOREIGN KEY (`MaintenanceID`) REFERENCES `maintenance` (`MaintenanceID`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `servicerecord`
+--
+
+LOCK TABLES `servicerecord` WRITE;
+/*!40000 ALTER TABLE `servicerecord` DISABLE KEYS */;
+/*!40000 ALTER TABLE `servicerecord` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `transaction`
+--
+
+DROP TABLE IF EXISTS `transaction`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `transaction` (
+  `TransactionID` int NOT NULL AUTO_INCREMENT,
+  `VIN` varchar(17) NOT NULL,
+  `CustomerID` int NOT NULL,
+  `date` varchar(20) NOT NULL,
+  `tax` double NOT NULL,
+  `paymentMethod` varchar(30) DEFAULT NULL,
+  PRIMARY KEY (`TransactionID`),
+  KEY `VIN` (`VIN`),
+  KEY `CustomerID` (`CustomerID`),
+  CONSTRAINT `transaction_ibfk_1` FOREIGN KEY (`VIN`) REFERENCES `vehicle` (`VIN`) ON DELETE CASCADE,
+  CONSTRAINT `transaction_ibfk_2` FOREIGN KEY (`CustomerID`) REFERENCES `customer` (`CustomerID`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `transaction`
+--
+
+LOCK TABLES `transaction` WRITE;
+/*!40000 ALTER TABLE `transaction` DISABLE KEYS */;
+/*!40000 ALTER TABLE `transaction` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `vehicle`
+--
+
+DROP TABLE IF EXISTS `vehicle`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `vehicle` (
+  `VIN` varchar(17) NOT NULL,
+  `model` varchar(50) NOT NULL,
+  `make` varchar(50) NOT NULL,
+  `year` int NOT NULL,
+  `color` varchar(20) NOT NULL,
+  `status` varchar(50) NOT NULL,
+  `currentCondition` varchar(30) NOT NULL,
+  `price` int DEFAULT NULL,
+  PRIMARY KEY (`VIN`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `vehicle`
+--
+
+LOCK TABLES `vehicle` WRITE;
+/*!40000 ALTER TABLE `vehicle` DISABLE KEYS */;
+/*!40000 ALTER TABLE `vehicle` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `warranty`
+--
+
+DROP TABLE IF EXISTS `warranty`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `warranty` (
+  `WarrantyID` int NOT NULL AUTO_INCREMENT,
+  `VIN` varchar(17) NOT NULL,
+  `type` varchar(30) NOT NULL,
+  `duration` int NOT NULL,
+  `policy` varchar(50) NOT NULL,
+  `price` double NOT NULL,
+  `coverageLimit` varchar(20) DEFAULT NULL,
+  PRIMARY KEY (`WarrantyID`),
+  KEY `VIN` (`VIN`),
+  CONSTRAINT `warranty_ibfk_1` FOREIGN KEY (`VIN`) REFERENCES `vehicle` (`VIN`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `warranty`
+--
+
+LOCK TABLES `warranty` WRITE;
+/*!40000 ALTER TABLE `warranty` DISABLE KEYS */;
+/*!40000 ALTER TABLE `warranty` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2024-11-18 16:58:22
